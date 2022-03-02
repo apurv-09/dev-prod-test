d1d38561891bff688f0c7c136272bc01f2526825
apurv-09 <84983056+apurv-09@users.noreply.github.com>
2022-03-02 13:05:37 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@ chmod 775 jojo
 whoami
 sudo touch /var/www/html/jojo
 sudo chmod 777 /var/www/html/jojo
-sudu -u apache chown jenkins:jenkins /var/www/html/my-project/abc
+sudo -u apache chown jenkins:jenkins /var/www/html/my-project/abc
